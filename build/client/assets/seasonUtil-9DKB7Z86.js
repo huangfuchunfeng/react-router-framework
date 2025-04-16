0216e4a4fa3d08769e500eef1f4c0b5f4f2fb14b
huangfuchunfeng <huangfuchunfeng@qq.com>
2025-04-16 15:03:29 +0800

    update

---
+++ build/client/assets/seasonUtil-9DKB7Z86.js
@@ -0,0 +1,1 @@
+function s(){return+(window.localStorage.getItem("open")||0)}function a(e){const n=Math.ceil(e/8),o=e-(n-1)*8;return{seasonNum:n,episodeNum:o}}function i(e,t){return(e-1)*8+t}export{a,i as b,s as g};
